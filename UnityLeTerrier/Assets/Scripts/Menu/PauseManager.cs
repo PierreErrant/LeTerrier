095e55f8a3432571a8599baf27ebce98f6bc123d
Pierre <PierreErrant@proton.me>
2025-02-06 16:03:15 +0100

    Final week 1

--- UnityLeTerrier/Assets/Scripts/Menu/PauseManager.cs
+++ UnityLeTerrier/Assets/Scripts/Menu/PauseManager.cs
@@ -1,26 +1,36 @@
 using System;
-using System.Diagnostics;
-using System.Security.Cryptography;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Debug = System.Diagnostics.Debug;
 
 
 public class PauseManager : MonoBehaviour
 {
-    private bool active = false;
-    [SerializeField] GameObject pauseMenu;
-    
+    private bool _active = false;
+    [SerializeField] GameObject _pauseMenu;
     public void Pause(InputAction.CallbackContext context)
     {
-        active = !active;
-        pauseMenu.SetActive(active);
-        if (active) Time.timeScale = 0.0f;
-        else Time.timeScale =  1;
+        Active = !Active;
+        _pauseMenu.SetActive(Active);
     }
-
     public void Quit()
     {
         Application.Quit();
-        UnityEngine.Debug.Log("Quit");
+    }
+    public bool Active // Permet de détecter les changements d'état
+    {
+        get {return _active;}
+        set 
+        {
+            if (_active != value)
+            {
+                _active = value;
+                { 
+                    float boolF = !_active ? 1.0f : 0.0f;
+                    Cursor.visible = _active;
+                    Time.timeScale = boolF;
+                }
+            }
+        } 
     }
 }
--- UnityLeTerrier/Assets/Scripts/SceneLoader.cs
+++ UnityLeTerrier/Assets/Scripts/SceneLoader.cs
@@ -8,7 +8,7 @@ public class SceneLoader : MonoBehaviour
     void Start()
     {
         LoadSceneList(scenesToLoad);
-        
+        Cursor.visible = false; 
     }
 
     // Update is called once per frame
